bc858b093d10879a57c8ad158de671b73cdc3656
민지 <97029619+mandoo15@users.noreply.github.com>
2024-04-09 19:23:13 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.04 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.java
+++ 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.java
@@ -11,10 +11,16 @@ PROGRAMMER-962식 나이 return
 String[] x라는 배열에 숫자를 문자로 바꿔 줄 알파벳들을 각 인덱스에 집어넣음. (인덱스 번호가 알파벳으로 바꾼 거랑 같음 )
 age를 string으로 변환해서 split("")해서 새로운 배열 ( str )에 저장.
 */
-import java.util.stream.Collectors;
 
 class Solution {
     public String solution(int age) {
-        return String.valueOf(age).chars().mapToObj(operand -> String.valueOf((char) (49 + operand))).collect(Collectors.joining());
+        StringBuilder sb = new StringBuilder();
+
+        while(age > 0) {
+            sb.insert(0, (char) ((age % 10) + (int)'a'));
+            age /= 10;
+        }
+
+        return sb.toString();
     }
 }
