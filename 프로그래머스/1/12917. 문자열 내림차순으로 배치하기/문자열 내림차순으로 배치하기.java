9b6f79efa1be2f2d99b2ee562f278153cd107591
민지 <97029619+mandoo15@users.noreply.github.com>
2024-04-10 19:00:33 +0900

    [level 1] Title: 문자열 내림차순으로 배치하기, Time: 0.04 ms, Memory: 70.1 MB -BaekjoonHub

--- 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.java
+++ 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.java
@@ -1,28 +1,20 @@
 class Solution {
-    public String solution(String s) {
-        String answer = "";
-        int[] arr = new int[s.length()];
-        
-        for(int i=0; i<s.length(); i++){ // ASCII코드 변환
-            char x = s.charAt(i);
-            arr[i] = Character.valueOf(x);
-        }
-        
-        for(int i=0; i<arr.length; i++){ // 내림차순 정렬
-            for(int j=0; j<arr.length-1; j++){
-                if(arr[j] < arr[j+1]){
-                    int tmp = arr[j+1];
-                    arr[j+1] = arr[j];
-                    arr[j] = tmp;
+    public String solution(String str) {
+        char[] chars = str.toCharArray();
+        char temp =' ';
+
+            for(int i = 0; i < str.length(); i++) {
+             for(int j = 0; j < str.length()-1 ; j++){
+                 if( chars[j] < chars[j+1]){
+                     temp = chars[j];
+                     chars[j] = chars[j+1];
+                     chars[j+1] = temp;
+                 }
                 }
             }
-        }
-        
-        for(int i=0; i<arr.length; i++){ // 알파벳 변환 및 문자열 만들기
-            char ch = (char)arr[i]; 
-            answer = answer + Character.toString(ch);
 
-        }
-        return answer;
+
+            str = new String(chars, 0, chars.length);
+            return str;
     }
 }
