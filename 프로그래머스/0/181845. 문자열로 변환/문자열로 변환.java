2b201443f5b047bac9c69a08ce823db2b97dd42a
민지 <97029619+mandoo15@users.noreply.github.com>
2024-04-16 14:21:40 +0900

    [level 0] Title: 문자열로 변환, Time: 0.02 ms, Memory: 73.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181845. 문자열로 변환/문자열로 변환.java
@@ -0,0 +1,11 @@
+/*
+정수 n이 주어질 때 n을 문자열로 변환하여 return
+--> HOW?
+Integer.toString 이용.
+*/
+class Solution {
+    public String solution(int n) {
+        String answer = Integer.toString(n);
+        return answer;
+    }
+}
