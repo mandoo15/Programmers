ded9658fd30a7625e94fd9668e83ce6418164b21
민지 <97029619+mandoo15@users.noreply.github.com>
2024-04-18 13:22:05 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 0.02 ms, Memory: 76.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int answer = 0, sum = 0, multi = 1;
+        for(int i=0; i<num_list.length; i++){
+            sum += num_list[i];
+            multi *= num_list[i];
+        }
+        if(sum*sum > multi) answer = 1;
+        return answer;
+    }
+}
