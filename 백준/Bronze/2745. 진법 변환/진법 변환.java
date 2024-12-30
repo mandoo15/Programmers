75ac15016cfbbd221dd9625f65b48d8e3ae188d4
민지 <97029619+mandoo15@users.noreply.github.com>
2024-12-30 13:46:36 +0900

    [Bronze II] Title: 진법 변환, Time: 184 ms, Memory: 17776 KB -BaekjoonHub

---
+++ 백준/Bronze/2745. 진법 변환/진법 변환.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        int x = sc.nextInt();
+        System.out.println(Integer.parseInt(s, x));
+    }
+}
