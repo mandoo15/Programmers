2b08a1f2ecea40a7c4d99766444650c2578e15a8
민지 <97029619+mandoo15@users.noreply.github.com>
2025-01-06 23:56:14 +0900

    [Silver V] Title: 수 정렬하기 4, Time: 4636 ms, Memory: 187796 KB -BaekjoonHub

---
+++ 백준/Silver/11931. 수 정렬하기 4/수 정렬하기 4.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(arr);
+        for (int i = n - 1; i > -1; i--) {
+            System.out.println(arr[i]);
+        }
+    }
+}
