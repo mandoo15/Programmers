32813339c5397754a2f009c396c29627548f3a67
민지 <97029619+mandoo15@users.noreply.github.com>
2025-01-06 23:18:14 +0900

    [Bronze IV] Title: R2, Time: 168 ms, Memory: 17636 KB -BaekjoonHub

---
+++ 백준/Bronze/3046. R2/R2.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+
+        int r1 = sc.nextInt();
+        int s = sc.nextInt();
+        System.out.println(2*s-r1);
+    }
+}
