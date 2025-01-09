06ef97871727e04e5ce95f1e6e08cd77135c77e2
민지 <97029619+mandoo15@users.noreply.github.com>
2025-01-09 14:24:21 +0900

    [Bronze III] Title: 직각삼각형, Time: 180 ms, Memory: 17880 KB -BaekjoonHub

---
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[3];
+
+        while (true) {
+            for (int i = 0; i < 3; i++) {
+                arr[i] = sc.nextInt();
+            }
+
+            if (arr[0] == 0 && arr[1] == 0 && arr[2] == 0) {
+                break;
+            }
+
+            Arrays.sort(arr);
+            if (arr[0] * arr[0] + arr[1] * arr[1] == arr[2] * arr[2]) {
+                System.out.println("right");
+            } else {
+                System.out.println("wrong");
+            }
+
+        }
+    }
+}
