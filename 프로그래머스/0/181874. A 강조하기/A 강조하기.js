38754dabc63979d8d10b33361eaf7b253b49b1fe
민지 <97029619+mandoo15@users.noreply.github.com>
2025-01-19 00:17:32 +0900

    [level 0] Title: A 강조하기, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.js
@@ -0,0 +1,30 @@
+function solution(myString) {
+    myString = myString.replaceAll("a", "A");
+    myString = myString.replaceAll("B", "b");
+    myString = myString.replaceAll("C", "c");
+    myString = myString.replaceAll("D", "d");
+    myString = myString.replaceAll("E", "e");
+    myString = myString.replaceAll("F", "f");
+    myString = myString.replaceAll("G", "g");
+    myString = myString.replaceAll("H", "h");
+    myString = myString.replaceAll("I", "i");
+    myString = myString.replaceAll("J", "j");
+    myString = myString.replaceAll("K", "k");
+    myString = myString.replaceAll("L", "l");
+    myString = myString.replaceAll("M", "m");
+    myString = myString.replaceAll("N", "n");
+    myString = myString.replaceAll("O", "o");
+    myString = myString.replaceAll("P", "p");
+    myString = myString.replaceAll("Q", "q");
+    myString = myString.replaceAll("R", "r");
+    myString = myString.replaceAll("S", "s");
+    myString = myString.replaceAll("T", "t");
+    myString = myString.replaceAll("U", "u");
+    myString = myString.replaceAll("V", "v");
+    myString = myString.replaceAll("W", "w");
+    myString = myString.replaceAll("X", "x");
+    myString = myString.replaceAll("Y", "y");
+    myString = myString.replaceAll("Z", "z");
+    var answer = myString;
+    return answer;
+}
--- 프로그래머스/0/181874. A 강조하기/README.md
+++ 프로그래머스/0/181874. A 강조하기/README.md
@@ -1,10 +1,10 @@
 # [level 0] A 강조하기 - 181874 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181874) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181874?language=javascript) 
 
 ### 성능 요약
 
-메모리: 77.6 MB, 시간: 9.65 ms
+메모리: 33.4 MB, 시간: 0.10 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 17일 19:11:47
+2025년 01월 19일 00:17:31
 
 ### 문제 설명
 
