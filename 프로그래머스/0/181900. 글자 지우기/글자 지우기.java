5c7c3c4e2c70da5f91e822eccb21d1907e34a444
민지 <97029619+mandoo15@users.noreply.github.com>
2025-03-10 13:31:26 +0900

    [level 0] Title: 글자 지우기, Time: 0.57 ms, Memory: 85.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+class Solution {
+    public String solution(String my_string, int[] indices) {
+        StringBuilder sb = new StringBuilder(my_string);
+        Arrays.sort(indices);
+
+        for (int i = 0; i < indices.length; i++) {
+            sb.deleteCharAt(indices[i] - i);
+        }
+        
+        String answer = sb.toString();
+        return answer;
+    }
+}
