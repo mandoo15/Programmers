3dbbdc41eb2a000f3a39b3ec2e8d9997083fd8d3
민지 <97029619+mandoo15@users.noreply.github.com>
2025-09-02 01:49:46 +0900

    [level 0] Title: 평행, Time: 0.03 ms, Memory: 85.9 MB -BaekjoonHub

--- 프로그래머스/0/120875. 평행/평행.java
+++ 프로그래머스/0/120875. 평행/평행.java
@@ -1,14 +1,19 @@
 class Solution {
     public int solution(int[][] dots) {
-        // (0,1) (2,3)
-        if (((dots[0][0] - dots[1][0]) * (dots[2][1] - dots[3][1])) == ((dots[0][1] - dots[1][1]) * (dots[2][0] - dots[3][0])) ) return 1;
+        // case 1: (0,1) vs (2,3)
+        if (isParallel(dots[0], dots[1], dots[2], dots[3])) return 1;
 
-        // (0,2) (1,3)
-        if (((dots[0][0] - dots[2][0]) * (dots[1][1] - dots[3][1])) == ((dots[0][1] - dots[2][1]) * (dots[1][0] - dots[3][0])) ) return 1;
+        // case 2: (0,2) vs (1,3)
+        if (isParallel(dots[0], dots[2], dots[1], dots[3])) return 1;
 
-        // (0,3) (1,2)
-        if (((dots[0][0] - dots[3][0]) * (dots[1][1] - dots[2][1])) == ((dots[0][1] - dots[3][1]) * (dots[1][0] - dots[2][0])) ) return 1;
+        // case 3: (0,3) vs (1,2)
+        if (isParallel(dots[0], dots[3], dots[1], dots[2])) return 1;
 
         return 0;
+    }
+
+    // 기울기 비교 대신 cross 곱으로 비교
+    private boolean isParallel(int[] a, int[] b, int[] c, int[] d) {
+        return (b[1] - a[1]) * (d[0] - c[0]) == (d[1] - c[1]) * (b[0] - a[0]);
     }
 }
