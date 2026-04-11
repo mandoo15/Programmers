7605ded0d2525d7e14694b2d80b38e2d3b23909a
민지 <97029619+mandoo15@users.noreply.github.com>
2026-04-11 18:12:36 +0900

    [Gold V] Title: 선 긋기, Time: 2300 ms, Memory: 60760 KB -BaekjoonHub

--- 백준/Gold/2170. 선 긋기/선 긋기.java
+++ 백준/Gold/2170. 선 긋기/선 긋기.java
@@ -1,50 +1,34 @@
 import java.util.*;
 import java.io.*;
 
-class Line implements Comparable<Line> { // 선 클래스
-    int start, end;
-    Line(int start, int end) {
-        this.start = start;
-        this.end = end;
-    }
-    
-    @Override // 정렬 규칙
-    public int compareTo(Line o) {
-        if(this.start == o.start) {
-            return Integer.compare(this.end, o.end);
-        }
-        return Integer.compare(this.start, o.start);
-    }
-}
-
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-
-        Line[] lines = new Line[N];
+        int N = readInt();
+        int[][] lines = new int[N][2];
 
         for (int i = 0; i < N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
-            lines[i] = new Line(x, y);
+            lines[i][0] = readInt();
+            lines[i][1] = readInt();
         }
 
-        Arrays.sort(lines);
+        Arrays.sort(lines, (a, b) -> {
+            if(a[0] == b[0]) return Integer.compare(a[1], b[1]);
+            return Integer.compare(a[0], b[0]);
+        });
 
         long totalLength = 0;
-        int currentStart = lines[0].start;
-        int currentEnd = lines[0].end;
+        int currentStart = lines[0][0];
+        int currentEnd = lines[0][1];
 
         for (int i = 1; i < N; i++) {
-            if (lines[i].start <= currentEnd) {
-                currentEnd = Math.max(currentEnd, lines[i].end);
+            if (lines[i][0] <= currentEnd) {
+                currentEnd = Math.max(currentEnd, lines[i][1]);
             } 
             else {
                 totalLength += (currentEnd - currentStart); 
-                currentStart = lines[i].start;
-                currentEnd = lines[i].end;
+                currentStart = lines[i][0];
+                currentEnd = lines[i][1];
             }
         }
         
@@ -52,5 +36,19 @@ public class Main {
         totalLength += (currentEnd - currentStart);
 
         System.out.println(totalLength);
+    }
+    
+    private static int readInt() throws Exception {
+        int c, n = 0;
+        boolean negative = false;
+        while((c = System.in.read()) <= 32); // 공백 무시
+        if (c == '-') {
+            negative = true;
+            c = System.in.read();
+        }
+        do {
+            n = n * 10 + (c & 15);
+        } while((c = System.in.read()) > 47 && c < 58);
+        return negative ? -n : n;
     }
 }
